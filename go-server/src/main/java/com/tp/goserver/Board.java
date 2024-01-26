031f6411730debded346de6a8ae926053e6af372
budzu1 <147948018+budzu1@users.noreply.github.com>
2024-01-26 22:01:09 +0100

    new file:   client-go/target/classes/com/TP/Goban.class 	modified:   client-go/target/classes/com/TP/MainScreen$1.class 	modified:   client-go/target/classes/com/TP/MainScreen$2.class 	modified:   client-go/target/classes/com/TP/MainScreen$3.class 	modified:   client-go/target/classes/com/TP/MainScreen.class 	new file:   client-go/target/classes/com/TP/Stone.class 	modified:   go-server/src/main/java/com/tp/goserver/Board.java 	new file:   go-server/src/main/java/com/tp/goserver/RuleChecker.java 	new file:   go-server/src/main/java/com/tp/goserver/Stone.java

--- go-server/src/main/java/com/tp/goserver/Board.java
+++ go-server/src/main/java/com/tp/goserver/Board.java
@@ -4,5 +4,24 @@ import java.util.ArrayList;
 
 public class Board {
 
-    ArrayList<String> moves = new ArrayList<>();
+    private ArrayList<ArrayList<Stone>> stones = new ArrayList<>();
+
+    public Board(int n) {
+        addStones(n);
+    }
+
+    private void addStones(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            ArrayList<Stone> tempArray = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+
+                tempArray.add(new Stone());
+            }
+
+            stones.add(tempArray);
+        }
+    }
 }
