4358242159742db1ec8b6a5e416ce876e6d01609
budzu1 <147948018+budzu1@users.noreply.github.com>
2024-01-27 23:47:45 +0100

    modified:   client-go/target/classes/com/TP/Goban.class 	modified:   client-go/target/classes/com/TP/MainScreen.class 	new file:   go-server/src/main/java/com/tp/goserver/BlackState.java 	new file:   go-server/src/main/java/com/tp/goserver/CountingState.java 	modified:   go-server/src/main/java/com/tp/goserver/GoGame.java 	new file:   go-server/src/main/java/com/tp/goserver/GoGameState.java 	new file:   go-server/src/main/java/com/tp/goserver/WaitingState.java 	new file:   go-server/src/main/java/com/tp/goserver/WhiteState.java

---
+++ go-server/src/main/java/com/tp/goserver/WaitingState.java
@@ -0,0 +1,25 @@
+package com.tp.goserver;
+
+public class WaitingState implements GoGameState {
+    @Override
+    public void addOpponent(GoGame game, String opponent) {
+        // Add logic to add opponent
+        game.setState(new BlackState());
+    }
+
+    @Override
+    public void addBot(GoGame game) {
+        // Add logic to add bot
+        game.setState(new BlackState());
+    }
+
+    @Override
+    public void addMove(GoGame game, String move) {
+        throw new IllegalStateException("Cannot add move in waiting state");
+    }
+
+    @Override
+    public void end(GoGame game) {
+        throw new IllegalStateException("Cannot end game in waiting state");
+    }
+}
